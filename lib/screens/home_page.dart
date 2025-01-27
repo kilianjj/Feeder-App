7111579257bcd7b398bc651e18410a8dfa51798a
Kilian Jakstis <kilianjj@Kilians-MBP.lan>
2025-01-26 23:35:58 -0500

    fixed minor issue from old code

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -24,13 +24,14 @@ class MainPageState extends State<MainPage> with WidgetsBindingObserver {
   int _currentIndex = 0;
   // db streams for last feed time and msg updates
   Stream<DatabaseEvent> _lastFeedStream =
-      FirebaseDatabaseService().lastRef.onValue.asBroadcastStream();
+  FirebaseDatabaseService().lastRef.onValue.asBroadcastStream();
   Stream<DatabaseEvent> _msgStream =
-    FirebaseDatabaseService().msgRef.onValue.asBroadcastStream();
+  FirebaseDatabaseService().msgRef.onValue.asBroadcastStream();
   Stream<DatabaseEvent> _connectionStream =
-      FirebaseDatabaseService().connectionRef.onValue.asBroadcastStream();
+  FirebaseDatabaseService().connectionRef.onValue.asBroadcastStream();
   StreamSubscription<DatabaseEvent>? _connectionSubscription;
   StreamSubscription<DatabaseEvent>? _msgSubscription;
+  StreamSubscription<DatabaseEvent>? _feedSub;
 
   // fetch updated info from db, make widget listen to streams for ui updates
   @override
@@ -38,13 +39,17 @@ class MainPageState extends State<MainPage> with WidgetsBindingObserver {
     FeedApi.getInfo();
     super.initState();
     WidgetsBinding.instance.addObserver(this);
+    // get last time
+    _feedSub = _lastFeedStream.listen((DatabaseEvent event) {
+      FeederState.cachedLastFeed = event.snapshot.value!.toString();
+    });
     // check connection status
     _connectionSubscription = _connectionStream.listen((DatabaseEvent event) {
       final bool isConnected = event.snapshot.value as bool? ?? false;
       // debugPrint("Firebase connected: $isConnected");
       if (!isConnected) {
         ScaffoldMessenger.of(context).showSnackBar(
-          ToastHelper.getToastBar("Lost Firebase connection...")
+            ToastHelper.getToastBar("Lost Firebase connection...")
         );
       }
     });
@@ -68,7 +73,7 @@ class MainPageState extends State<MainPage> with WidgetsBindingObserver {
   void addSelectedTime(TimeOfDay selectedTime) async {
     String formattedTime = TimeConverter.convertToBoardTime(selectedTime);
     await FeederState.addTime(formattedTime,
-        (s) {
+            (s) {
           if (s) {
             FeederState.cachedSavedTimes.add(formattedTime);
           }
@@ -90,7 +95,7 @@ class MainPageState extends State<MainPage> with WidgetsBindingObserver {
   // shows feeder response msg
   void triggerFeed() {
     FeederState.feedNow(
-        (s) {
+            (s) {
         }
     );
   }
@@ -158,6 +163,7 @@ class MainPageState extends State<MainPage> with WidgetsBindingObserver {
     // cancel the stream subscriptions
     _msgSubscription?.cancel();
     _connectionSubscription?.cancel();
+    _feedSub?.cancel();
     WidgetsBinding.instance.removeObserver(this);
     super.dispose();
   }
@@ -166,54 +172,54 @@ class MainPageState extends State<MainPage> with WidgetsBindingObserver {
   Widget feedWidget(String lastFeed) {
     String formattedTime = TimeConverter.parseBoardTime(lastFeed);
     return Center(
-      child: Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            'Last fed: ${formattedTime}',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w400,
-              // color: Theme.of(context).colorScheme.secondary,
-              color: Colors.black
-            ),
-          ),
-          SizedBox(height: 40),
-          ElevatedButton(
-            onPressed: triggerFeed,
-            style: ElevatedButton.styleFrom(
-              minimumSize: Size(200, 60),
-              shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30),
-              ),
-              backgroundColor: Colors.white70,
-              textStyle: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500
-              ),
-            ),
-            child: Text('Feed now'),
-          ),
-        ])
-      )
+        child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Last fed: ${formattedTime}',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                        // color: Theme.of(context).colorScheme.secondary,
+                        color: Colors.black
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  ElevatedButton(
+                    onPressed: triggerFeed,
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(200, 60),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      backgroundColor: Colors.white70,
+                      textStyle: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500
+                      ),
+                    ),
+                    child: Text('Feed now'),
+                  ),
+                ])
+        )
     );
   }
-  
+
   // builds home page info widget based on feeder state in db
   Widget buildStreamHomePage() {
     return StreamBuilder<DatabaseEvent>(
-      stream: _lastFeedStream,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Text("Error fetching data");
-        } else if (snapshot.hasData) {
-          FeederState.cachedLastFeed = snapshot.data!.snapshot.value.toString();
+        stream: _lastFeedStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text("Error fetching data");
+          } else if (snapshot.hasData) {
+            FeederState.cachedLastFeed = snapshot.data!.snapshot.value.toString();
+          }
+          return feedWidget(FeederState.cachedLastFeed);
         }
-        return feedWidget(FeederState.cachedLastFeed);
-      }
     );
   }
 }
